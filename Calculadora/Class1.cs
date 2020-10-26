ccdb2b29a500560789d24ae043d01064ecadea68
Luiz Felipe Pereira bandeira <68253749+luizfelipejs@users.noreply.github.com>
2020-10-26 15:43:21 -0300

    upload files

---
+++ Calculadora/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso_Excript
+{
+  class Calculadora
+    {
+        public int Soma(int x, int y) => x += 1;
+        public int Subtracao(int x, int y) => x -= 1;
+
+        public int Numero { get; }
+    }
+
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Curso_Excript
+{
+    class Program
+    {
+        static public Boolean verifyString (string value)
+        {
+            if (value.ToLower() == "n")
+            {
+                return true;
+            } else if (value.ToLower() == "s")
+            {
+                return false;
+            }
+            return false;
+        }
+        static void Main()
+        {
+            Boolean stop_value = false;
+            Calculadora calculadora = new Calculadora();
+
+            while(stop_value == false)
+            {
+                Console.WriteLine("Digite um valor:");
+                
+                int x = Int32.Parse(Console.ReadLine());
+                int y = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"o resultado de {x} + {y} é {calculadora.Soma(x, y)}");
+                Console.WriteLine("Digite Se quer continuar [S/N]");
+                
+                stop_value = verifyString(Console.ReadLine());
+            };
+        }
+    }
+}
